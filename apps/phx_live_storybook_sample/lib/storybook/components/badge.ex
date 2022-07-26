c0a9f9d71b6f58c8c23fea6dc1af030eec2bf102
Christian Blavier <cblavier@gmail.com>
2022-07-26 18:10:22 +0200

    lot of badges!

--- apps/phx_live_storybook_sample/lib/storybook/components/badge.ex
+++ apps/phx_live_storybook_sample/lib/storybook/components/badge.ex
@@ -9,39 +9,42 @@ defmodule Storybook.Components.Badge do
   def icon, do: "fal fa-badge-check"
 
   def variations do
-    [
-      %Variation{
-        id: :default_badge,
-        attributes: %{
-          :label => "A badge"
+    for i <- 1..5 do
+      [
+        %Variation{
+          id: :"default_badge_#{i}",
+          attributes: %{
+            :label => "A badge"
+          }
+        },
+        %Variation{
+          id: :"with_custom_colors_#{i}",
+          attributes: %{
+            :label => "An indigo badge",
+            :"text-color" => "text-white",
+            :"bg-color" => "bg-indigo-600"
+          }
+        },
+        %Variation{
+          id: :"with_an_icon_#{i}",
+          description: "With a custom FontAwesome icon",
+          attributes: %{
+            :label => "A check badge",
+            :icon => "fa fa-check"
+          }
+        },
+        %Variation{
+          id: :"with_color_and_icon_#{i}",
+          description: "With a custom FontAwesome icon & color",
+          attributes: %{
+            :label => "Danger",
+            :icon => "fas fa-skull",
+            :"text-color" => "text-red-700",
+            :"bg-color" => "bg-red-200"
+          }
         }
-      },
-      %Variation{
-        id: :with_custom_colors,
-        attributes: %{
-          :label => "An indigo badge",
-          :"text-color" => "text-white",
-          :"bg-color" => "bg-indigo-600"
-        }
-      },
-      %Variation{
-        id: :with_an_icon,
-        description: "With a custom FontAwesome icon",
-        attributes: %{
-          :label => "A check badge",
-          :icon => "fa fa-check"
-        }
-      },
-      %Variation{
-        id: :with_color_and_icon,
-        description: "With a custom FontAwesome icon",
-        attributes: %{
-          :label => "Danger",
-          :icon => "fas fa-skull",
-          :"text-color" => "text-red-700",
-          :"bg-color" => "bg-red-200"
-        }
-      }
-    ]
+      ]
+    end
+    |> List.flatten()
   end
 end
