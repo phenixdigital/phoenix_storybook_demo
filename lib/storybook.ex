f8baa88f969d6dd7aa7042c6340086a0e5c25a6b
Christian Blavier <cblavier@gmail.com>
2026-09-07 10:37:26 +0200

    fixed deployment

--- lib/storybook.ex
+++ lib/storybook.ex
@@ -19,5 +19,6 @@ defmodule Storybook do
     color_mode: true,
     font_awesome_plan: :pro,
     font_awesome_kit_id: "7a72374eed",
-    source_permalink_base_url: "https://github.com/phenixdigital/phoenix_storybook_demo/blob/main"
+    source_permalink_base_url: "https://github.com/phenixdigital/phoenix_storybook_demo",
+    source_permalink_root: "/app"
 end
